773dc07f8f6005331c3f16579769dfab4034d511
Wieslaw <Wieslaw@Thirteen37>
2015-06-02 09:59:40 -0400

    Merge origin/master
    Conflicts:
    	Lab3/site/config/config.ini.php
    	Lab3/site/logs/errors.log.php
    	Lab3/site/logs/exception.log.php
    	Lab3/site/mvc/controllers/BaseController.php
    	Lab3/site/mvc/controllers/EmailtypeController.php
    	Lab3/site/mvc/controllers/IndexController.php
    	Lab3/site/mvc/controllers/Page404Controller.php
    	Lab3/site/mvc/models/dao/BaseDAO.php
    	Lab3/site/mvc/models/dao/EmailTypeDAO.php
    	Lab3/site/mvc/models/do/BaseModel.php
    	Lab3/site/mvc/models/do/EmailTypeModel.php
    	Lab3/site/mvc/models/exceptions/PageNotFoundException.php
    	Lab3/site/mvc/models/exceptions/ScopeVariableNotFoundException.php
    	Lab3/site/mvc/models/interfaces/IController.php
    	Lab3/site/mvc/models/interfaces/IDAO.php
    	Lab3/site/mvc/views/index.php

--- Lab3/site/mvc/models/exceptions/PageNotFoundException.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace App\models\services;
-
-use Exception;
-class PageNotFoundException extends Exception {}
--- Lab3/site/mvc/models/exceptions/ScopeVariableNotFoundException.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace App\models\services;
-
-use Exception;
-class ScopeVariableNotFoundException extends Exception {}
